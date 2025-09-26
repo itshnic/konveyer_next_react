8118e49736613fa78a14296f71611dd3f915d514
Itshnic72 <rv920490@gmail.com>
2025-09-26 15:48:21 +0500

    new progect

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,15 @@
+import { Libre_Franklin, Open_Sans, Mulish } from "next/font/google";
+export const libreFranklin = Libre_Franklin({
+  subsets: ["latin"],
+	weight:['400','600','700']
+});
+
+export const openSans = Open_Sans({
+  subsets: ["latin"],
+	weight:['400','600','700']
+});
+
+export const mulish = Mulish({
+  subsets: ["latin"],
+	weight:['400','600','700']
+});
