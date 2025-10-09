8bc0b772ab5ca0f1cfb66148004859f9ea63b474
Itshnic72 <rv920490@gmail.com>
2025-10-09 15:31:30 +0500

    popUp update

--- src/app/ui/components/popUp.tsx
+++ src/app/ui/components/popUp.tsx
@@ -3,23 +3,31 @@ import Link from "next/link";
 import { useState, useEffect } from "react";
 
 export default function PopUp() {
-	const [vis, setVis] = useState("hidden");
+	const [visible, setVisible] = useState(true);
 
-	function getBlock() {
-		if (vis == "hidden") {
-			setVis("block");
-		} else setVis("hidden");
-	}
 	return (
-		<div className="text-[white]  bg-[#23272C] p-[5px]">
+		<div className="text-[white] bg-[#23272C] p-[5px] ">
 			<span
-				onClick={getBlock}
-				className="font-bold after:content-['<'] cursor-pointer"
+				onClick={() => (visible ? setVisible(false) : setVisible(true))}
+				className={`font-bold cursor-pointer flex gap-1.5 self-center hover:text-[#9c9898] active:text-[white] transition duration-200 ease-in`}
 			>
 				Табель
+				<svg
+					className={`fill-current w-4 transition duration-300 ${
+						visible ? `rotate-0` : `-rotate-180`
+					}`}
+					viewBox="0 0 52 52"
+				>
+					<path d="M47.6,17.8L27.1,38.5c-0.6,0.6-1.6,0.6-2.2,0L4.4,17.8c-0.6-0.6-0.6-1.6,0-2.2l2.2-2.2c0.6-0.6,1.6-0.6,2.2,0l16.1,16.3c0.6,0.6,1.6,0.6,2.2,0l16.1-16.2c0.6-0.6,1.6-0.6,2.2,0l2.2,2.2C48.1,16.3,48.1,17.2,47.6,17.8z" />
+				</svg>
 			</span>
-			<ul className={`p-[5px] ${vis}`}>
-				<li className="">
+
+			<ul
+				className={`p-[10px] flex flex-col gap-2 transition duration-300 ease-in absolute top-8 left-0 shadow-xl/30  bg-[#23272C] ${
+					visible ? `opacity-0 -top-100` : `opacity-100 top-8`
+				}`}
+			>
+				<li className="hover:text-[#9c9898] transition duration-200 ease-in">
 					<Link href="https://online-workvista.ru/indicators">Операторы</Link>
 				</li>
 				<li>
