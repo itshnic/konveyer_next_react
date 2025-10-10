09d69021bf0ac779cb005288a0f50d79e42aab23
Itshnic72 <rv920490@gmail.com>
2025-10-10 15:56:52 +0500

    color svg

--- src/app/ui/components/popUp.tsx
+++ src/app/ui/components/popUp.tsx
@@ -1,29 +1,34 @@
 "use client";
 import Link from "next/link";
+import Image from "next/image";
 import { useState, useEffect } from "react";
+import MySVG from "@/app/ui/components/svgReact.js";
 
 export default function PopUp() {
 	const [visible, setVisible] = useState(true);
 
 	return (
 		<div className="text-[white] bg-[#23272C] p-[5px] ">
-			<span
-				onClick={() => (visible ? setVisible(false) : setVisible(true))}
-				className={`font-bold cursor-pointer flex gap-1.5 self-center hover:text-[#9c9898] active:text-[white] transition duration-200 ease-in`}
-			>
-				Табель
-				<svg
-					className={`fill-current w-4 transition duration-300 ${
-						visible ? `rotate-0` : `-rotate-180`
-					}`}
-					viewBox="0 0 52 52"
+			<div className="flex">
+				<MySVG />
+				<span
+					onClick={() => (visible ? setVisible(false) : setVisible(true))}
+					className={`font-bold cursor-pointer flex gap-1.5 self-center hover:text-[#9c9898] active:text-[white] transition duration-200 ease-in`}
 				>
-					<path d="M47.6,17.8L27.1,38.5c-0.6,0.6-1.6,0.6-2.2,0L4.4,17.8c-0.6-0.6-0.6-1.6,0-2.2l2.2-2.2c0.6-0.6,1.6-0.6,2.2,0l16.1,16.3c0.6,0.6,1.6,0.6,2.2,0l16.1-16.2c0.6-0.6,1.6-0.6,2.2,0l2.2,2.2C48.1,16.3,48.1,17.2,47.6,17.8z" />
-				</svg>
-			</span>
+					Табель
+					<svg
+						className={`fill-current w-4 transition duration-300 ${
+							visible ? `rotate-0` : `-rotate-180`
+						}`}
+						viewBox="0 0 52 52"
+					>
+						<path d="M47.6,17.8L27.1,38.5c-0.6,0.6-1.6,0.6-2.2,0L4.4,17.8c-0.6-0.6-0.6-1.6,0-2.2l2.2-2.2c0.6-0.6,1.6-0.6,2.2,0l16.1,16.3c0.6,0.6,1.6,0.6,2.2,0l16.1-16.2c0.6-0.6,1.6-0.6,2.2,0l2.2,2.2C48.1,16.3,48.1,17.2,47.6,17.8z" />
+					</svg>
+				</span>
+			</div>
 
 			<ul
-				className={`p-[10px] flex flex-col gap-2 transition duration-300 ease-in absolute top-8 left-0 shadow-xl/30  bg-[#23272C] ${
+				className={`p-[10px] flex flex-col gap-2 transition duration-300 ease-in absolute top-8 left-0 shadow-xl/30  bg-[#23272C] rounded-sm ${
 					visible ? `opacity-0 -top-100` : `opacity-100 top-8`
 				}`}
 			>
---
+++ src/app/ui/components/svgReact.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { ReactSVG } from "react-svg";
+
+const MySVG = () => {
+	return <ReactSVG src="/icons/tools.svg" />;
+};
+
+export default MySVG;
